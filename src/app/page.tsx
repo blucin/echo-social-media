23c2edaf041f4b20c442eb1674e4f7d11cbc3549
Akshar Patel <glaivesharp@gmail.com>
2024-01-22 01:10:23 +0530

    Add warning about login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,10 @@ export default async function Home() {
   const session = await auth();
   return (
     <main className="flex min-h-screen items-center justify-center gap-3 p-24">
-      <h1>Echo, A Social Media Application</h1>
+      <div className="flex flex-col text-center mr-5">
+        <h1>Echo, A Social Media Application</h1>
+        <p> ⚠️ Make Login work with vercel, currently works for localhost only. </p>
+      </div>
       {session ? (
         <>
           <SignOutBtn />
