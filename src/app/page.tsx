2ed370f1f90696757311fe1783996a97a239a137
Akshar Patel <glaivesharp@gmail.com>
2024-01-22 01:01:19 +0530

    Add login buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import { Button } from "@/components/ui/button";
+import { auth } from "@/auth";
+import { SignInBtn, SignOutBtn } from "@/components/LoginBtns";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <main className="flex min-h-screen items-center justify-center gap-3 p-24">
-      <h1>Shadcn UI Test</h1>
-      <Button>Click Me</Button>
+      <h1>Echo, A Social Media Application</h1>
+      {session ? (
+        <>
+          <SignOutBtn />
+          <pre>
+            <code>{JSON.stringify(session, null, 2)}</code>
+          </pre>
+        </>
+      ) : (
+        <SignInBtn />
+      )}
     </main>
   );
 }
