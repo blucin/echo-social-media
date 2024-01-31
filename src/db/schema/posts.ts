50a41f8b90dcf946fa2733bed779afa58e1dfeac
Akshar Patel <glaivesharp@gmail.com>
2024-01-31 17:12:38 +0530

    Remove title field from posts table

--- src/db/schema/posts.ts
+++ src/db/schema/posts.ts
@@ -7,7 +7,6 @@ import { users } from "./users";
 
 export const posts = pgTable("post", {
   id: text("id").notNull().primaryKey(),
-  title: text("title").notNull(),
   content: text("content").notNull(),
   userId: text("userId").notNull().references(() => users.id, { onDelete: "cascade" }),
   createdAt: timestamp("createdAt").notNull().defaultNow(),
