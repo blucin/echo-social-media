c275866aad4bb171027f0c7950e2c2f96cb4c3b1
Akshar Patel <glaivesharp@gmail.com>
2024-02-01 12:20:26 +0530

    Remove unused imports and commented code

--- src/app/(routes-with-layout)/home/page.tsx
+++ src/app/(routes-with-layout)/home/page.tsx
@@ -1,16 +1,4 @@
 import { auth } from "@/auth";
-import { SignInBtn, SignOutBtn } from "@/components/LoginBtns";
-
-// top bar
-import { Button } from "@/components/ui/button";
-import {
-  AudioLinesIcon,
-  HomeIcon,
-  CompassIcon,
-  BellIcon,
-  SettingsIcon,
-  FeatherIcon,
-} from "lucide-react";
 
 import Post from "@/components/Post";
 
@@ -18,23 +6,6 @@ export default async function Home() {
   const session = await auth();
   return (
     <main className="flex flex-col">
-      {/*
-      <div className="flex flex-col text-center mr-5">
-        <h1>Echo, A Social Media Application</h1>
-        <p>
-          {" "}
-          ⚠️ Make Login work with vercel, currently works for localhost only.{" "}
-        </p>
-      </div>
-      {session ? (
-        <>
-          <SignOutBtn />
-          <code>{JSON.stringify(session, null, 4)}</code>
-        </>
-      ) : (
-        <SignInBtn />
-      )}
-      */}
       {[...Array(10)].map((x,i)=>
         <Post key={i} />
       )}
