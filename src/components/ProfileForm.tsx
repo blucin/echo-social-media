ebd4bdd4c519fe6a5e7f13caa47eae1d5af71ab1
Akshar Patel <glaivesharp@gmail.com>
2024-02-08 19:19:00 +0530

    Refactor form schema to separate global file

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -16,63 +16,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
-const MAX_FILE_SIZE = 1 * 1024 * 1024; // 1MB
-const ACCEPTED_IMAGE_TYPES = ["jpeg", "jpg", "png"];
-const formSchema = z.object({
-  username: z
-    .string()
-    .min(3, {
-      message: "Username must be at least 3 characters long",
-    })
-    .refine(
-      (username) => {
-        if (username.length > 20) {
-          return "Username must be at most 20 characters long";
-        }
-        return /^[a-zA-Z0-9_]+$/.test(username);
-      },
-      {
-        message: "Username can only contain letters, numbers, and underscores",
-      }
-    ),
-  bio: z
-    .union([
-      z
-        .string()
-        .length(
-          0,
-          "Bio needs to contain atleast 4 characters or atmost of 100 characters"
-        ),
-      z
-        .string()
-        .min(4)
-        .max(
-          100,
-          "Bio needs to contain atleast 4 characters or atmost of 100 characters"
-        ),
-    ])
-    .optional(),
-  thumbnail: z
-    .union([
-      z.undefined(),
-      z
-        .any()
-        .optional()
-        .refine(
-          (file) => file instanceof File || file instanceof FileList,
-          "Please upload a valid file."
-        )
-        .refine(
-          (file) => ACCEPTED_IMAGE_TYPES.includes(file.type.split("/")[1]),
-          "File type must be one of .jpeg, .jpg, .png"
-        )
-        .refine(
-          (file) => file.size <= MAX_FILE_SIZE,
-          "File size must not exceed the maximum file size."
-        ),
-    ])
-    .optional(),
-});
+import { ProfileFormSchema as formSchema } from "@/formSchemas";
 
 export default function ProfileForm() {
   const form = useForm<z.infer<typeof formSchema>>({
