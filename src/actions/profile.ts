616768cdabdc82b42408bf1378bae2c571a743db
Akshar Patel <glaivesharp@gmail.com>
2024-02-10 19:58:07 +0530

    Refactor profile actions to update user details

--- src/actions/profile.ts
+++ src/actions/profile.ts
@@ -1,15 +1,44 @@
 "use server";
 
+import { redirect } from 'next/navigation'
+
+import db from "@/lib/db";
+import { user } from "@/drizzle/out/schema";
+import { auth } from "@/auth";
+import { eq } from "drizzle-orm";
+
 // used to create a profile for the first time
 export const createProfile = async (data: {
   username: string;
   bio: string | undefined;
 }) => {
-  await setTimeout(() => {
-    console.log(data);
-  },1000);
-  if(false) {
-    return { error: "Profile already exists" };
+  const session = await auth();
+  if (!session) {
+    redirect("/signin");
   }
-  return { success: "Profile created" };
-}
+
+  const existingUser = await db
+    .select()
+    .from(user)
+    .where(eq(user.username, data.username));
+
+  if (existingUser.length > 0) {
+    return { error: "Username already exists" };
+  }
+
+  const res = await db
+    .update(user)
+    .set({ username: data.username, bio: data.bio })
+    .where(eq(user.id, session.user.id))
+    .returning({ username: user.username, bio: user.bio });
+
+  if (res.length === 0) {
+    return { error: "Failed to create profile" };
+  }
+  
+  if (!res || res[0].username == null) {
+    return { error: "Failed to create profile" };
+  }
+
+  redirect("/home");
+};
