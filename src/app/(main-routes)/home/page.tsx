795c2c61d024e583000f3f010dd23ef6a988ff27
Akshar Patel <glaivesharp@gmail.com>
2024-02-13 13:13:33 +0530

    Remove unnecessary auth hook

--- src/app/(main-routes)/home/page.tsx
+++ src/app/(main-routes)/home/page.tsx
@@ -1,9 +1,6 @@
-import { auth } from "@/auth";
-
 import Post from "@/components/Post";
 
 export default async function Home() {
-  const session = await auth();
   return (
     <main className="flex flex-col">
       {[...Array(10)].map((x,i)=>
