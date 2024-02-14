05956230a46b809033ff32fe5a42dd1ef31b2230
Akshar Patel <glaivesharp@gmail.com>
2024-02-15 00:23:52 +0530

    Drizzle introspect to update schema with removed title in post schema

--- src/drizzle/out/schema.ts
+++ src/drizzle/out/schema.ts
@@ -1,12 +1,6 @@
-import { pgTable, foreignKey, pgEnum, text, timestamp, unique, primaryKey, integer } from "drizzle-orm/pg-core"
-import { sql } from "drizzle-orm"
+import { pgTable, foreignKey, text, timestamp, unique, primaryKey, integer } from "drizzle-orm/pg-core"
+  import { sql } from "drizzle-orm"
 
-export const keyStatus = pgEnum("key_status", ['expired', 'invalid', 'valid', 'default'])
-export const keyType = pgEnum("key_type", ['stream_xchacha20', 'secretstream', 'secretbox', 'kdf', 'generichash', 'shorthash', 'auth', 'hmacsha256', 'hmacsha512', 'aead-det', 'aead-ietf'])
-export const factorType = pgEnum("factor_type", ['webauthn', 'totp'])
-export const factorStatus = pgEnum("factor_status", ['verified', 'unverified'])
-export const aalLevel = pgEnum("aal_level", ['aal3', 'aal2', 'aal1'])
-export const codeChallengeMethod = pgEnum("code_challenge_method", ['plain', 's256'])
 
 
 export const session = pgTable("session", {
@@ -42,7 +36,6 @@ export const comment = pgTable("comment", {
 
 export const post = pgTable("post", {
 	id: text("id").primaryKey().notNull(),
-	title: text("title").notNull(),
 	content: text("content").notNull(),
 	userId: text("userId").notNull().references(() => user.id, { onDelete: "cascade" } ),
 	createdAt: timestamp("createdAt", { mode: 'string' }).defaultNow().notNull(),
