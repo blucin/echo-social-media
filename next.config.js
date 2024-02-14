f7e3855fdf650cda3f7a12d7d1853780a2a9a71c
Akshar Patel <glaivesharp@gmail.com>
2024-02-14 23:31:13 +0530

    Add remotePatterns for google profile images

--- next.config.js
+++ next.config.js
@@ -9,6 +9,16 @@ const nextConfig = {
             }
         ]
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**'
+            }
+        ],
+    },
     output: 'standalone',
 }
 
