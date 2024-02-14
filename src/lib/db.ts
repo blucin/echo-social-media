8c229a5d83b76968335108635cae8e94191df357
Akshar Patel <glaivesharp@gmail.com>
2024-02-14 23:32:02 +0530

    Add table schemas to database initialization

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,21 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
-import { sql } from '@vercel/postgres';
+import { sql } from "@vercel/postgres";
 
-const db = drizzle(sql);
+// table schemas
+import * as userSchema from "@/db/schema/users";
+import * as postSchema from "@/db/schema/posts";
+import * as commentSchema from "@/db/schema/comments";
+import * as followSchema from "@/db/schema/followers";
+import * as likeSchema from "@/db/schema/likes";
+
+const db = drizzle(sql, {
+  schema: {
+    ...userSchema,
+    ...postSchema,
+    ...commentSchema,
+    ...followSchema,
+    ...likeSchema,
+  },
+});
 
 export default db;
