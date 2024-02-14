5256ed55b63ea052741a243984ac43d1e1a9c6e3
Akshar Patel <glaivesharp@gmail.com>
2024-02-14 23:24:39 +0530

    Remove unused getAccountById query

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -1,11 +1,7 @@
 import db from "@/lib/db";
-import { user, account } from "@/drizzle/out/schema";
+import { user } from "@/drizzle/out/schema";
 import { eq } from "drizzle-orm";
 
 export async function getUserById(id: string) {
   return db.select().from(user).where(eq(user.id, id));
-}
-
-export async function getAccountById(id: string) {
-  return db.select().from(account).where(eq(user.id, id));
 }
