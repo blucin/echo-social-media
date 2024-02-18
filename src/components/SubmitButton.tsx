2d042f8c656d20aa056b44f31c68da1699247149
Akshar Patel <glaivesharp@gmail.com>
2024-02-18 14:13:52 +0530

    Add optional re-validation query key prop
    Generally, after a form submission. We want to re-validate a path. However re-validation can only be done using react query if the data is being managed by it.

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -3,14 +3,21 @@
 import { useFormStatus } from "react-dom";
 import { Button, ButtonProps } from "./ui/button";
 import { Loader2 } from "lucide-react";
+import { useQueryClient } from "@tanstack/react-query";
 
 export interface SubmitButtonProps extends ButtonProps {
   text?: string;
+  revalidateQueryKey?: string | string[];
 }
 
 export default function SubmitButton({ ...props }: SubmitButtonProps) {
   const { pending } = useFormStatus();
+  const queryClient = useQueryClient();
 
+  if (!pending) {
+    queryClient.invalidateQueries({ queryKey: [props.revalidateQueryKey]});
+  }
+  
   return (
     <Button type="submit" aria-disabled={pending} disabled={pending} {...props}>
       {pending ? (
