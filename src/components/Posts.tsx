af16cfc2af009046d3b38e0b7397447e292cc0cc
Akshar Patel <glaivesharp@gmail.com>
2024-02-18 15:44:10 +0530

    Refactor to add user details and loading state

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -5,10 +5,13 @@ import { Button } from "@/components/ui/button";
 import { HeartIcon, ReplyIcon, RepeatIcon } from "lucide-react";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { type ResponseData } from "@/app/api/posts/fetch/route";
+import LoadingPosts from "@/components/LoadingPosts";
 
 const fetchPosts = async ({ pageParam = 0, limit = 2 }) => {
   try {
-    const res = await fetch(`/api/posts/fetch?page=${pageParam}&limit=${limit}`);
+    const res = await fetch(
+      `/api/posts/fetch?page=${pageParam}&limit=${limit}`
+    );
     const data: ResponseData = await res.json();
     return data;
   } catch (error) {
@@ -31,7 +34,7 @@ export default function Posts() {
     });
 
   if (isLoading) {
-    return <div className="pt-4 pb-1">Loading...</div>;
+    return <LoadingPosts postCnt={postsPerLoad}/>;
   }
 
   if (isError) {
@@ -42,36 +45,43 @@ export default function Posts() {
     return <div className="pt-4 pb-1">No data</div>;
   }
 
-  console.log(data.pages)
-
   return (
     <>
       {data.pages.map((page, i) => (
         <div key={i} className="pt-4 pb-1 flex flex-col gap-3">
-          {page.map((post) => (
-            <div key={post.id} className="px-5 flex-1 border-b">
-              <div className="flex items-center gap-2">
-                <span className="font-semibold">Username</span>
-                <span className="text-gray-500">@username</span>
-              </div>
-              <p className="mt-2 text-pretty">{post.content}</p>
-              <div className="mt-2 flex items-center gap-2">
-                <Button className="flex gap-1 items-center" variant="ghost">
-                  <HeartIcon className="h-4 w-4" />
-                  <span className="sr-only">Like</span>
-                  <span className="text-xs"> 0 </span>
-                </Button>
+          {page.map(({ user, post }) => (
+            <div key={post.id} className="flex items-start gap-3 px-5 pb-2 border-b">
+              <Image
+                src={user.image ? user.image : "/default-profile-pic.png"}
+                alt="avatar"
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+              <div className="flex-1">
+                <div className="flex items-center gap-2">
+                  <span className="font-semibold">{user.name}</span>
+                  <span className="text-gray-500">@{user.username}</span>
+                </div>
+                <p className="mt-2 text-pretty">{post.content}</p>
+                <div className="mt-2 flex items-center gap-2">
+                  <Button className="flex gap-1 items-center" variant="ghost">
+                    <HeartIcon className="h-4 w-4" />
+                    <span className="sr-only">Like</span>
+                    <span className="text-xs"> 0 </span>
+                  </Button>
 
-                <Button className="flex gap-1 items-center" variant="ghost">
-                  <ReplyIcon className="h-4 w-4" />
-                  <span className="sr-only">Comment</span>
-                  <span className="text-xs"> 69 </span>
-                </Button>
+                  <Button className="flex gap-1 items-center" variant="ghost">
+                    <ReplyIcon className="h-4 w-4" />
+                    <span className="sr-only">Comment</span>
+                    <span className="text-xs"> 69 </span>
+                  </Button>
 
-                <Button size="icon" variant="ghost">
-                  <RepeatIcon className="h-4 w-4" />
-                  <span className="sr-only">Retweet</span>
-                </Button>
+                  <Button size="icon" variant="ghost">
+                    <RepeatIcon className="h-4 w-4" />
+                    <span className="sr-only">Retweet</span>
+                  </Button>
+                </div>
               </div>
             </div>
           ))}
