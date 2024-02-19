3b6418afd5e44ac9632957d5cee2f003ba1661b7
Akshar Patel <glaivesharp@gmail.com>
2024-02-20 00:03:17 +0530

    Add conditional rendering for PostForm component

--- src/app/(main-routes)/home/page.tsx
+++ src/app/(main-routes)/home/page.tsx
@@ -1,10 +1,20 @@
 import Posts from "@/components/Posts";
 import PostForm from "@/components/PostForm";
+import { auth } from "@/auth";
+import { SignInBtn } from "@/components/LoginBtns";
 
 export default async function Home() {
+  const session = await auth();
   return (
     <>
-      <PostForm />
+      {session ? (<PostForm userImage={session.user.image}/>) : (
+        <div className="xl:hidden flex flex-col gap-3 px-5 py-10 border-b">
+        <p className="text-center text-gray-400">Log in to post</p>
+        <div className="flex justify-center">
+          <SignInBtn />
+        </div>
+      </div>
+      )}
       <main className="flex flex-col">
         <Posts postsPerLoad={5} />
       </main>
