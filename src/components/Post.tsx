7365fa37509ae7d71dc4cf2ccfa0f339ef77b943
Akshar Patel <glaivesharp@gmail.com>
2024-02-19 18:11:52 +0530

    Refactor Post component to use props and display author information

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -3,43 +3,40 @@ import { Button } from "@/components/ui/button";
 import { HeartIcon, ReplyIcon, RepeatIcon } from "lucide-react";
 
 type PostProps = {
+  id: string;
   content: string;
   authorName: string;
+  authorUsername: string;
   authorImage: string | null | undefined;
-  authorHandle: string;
 };
 
-export default function Post() {
+export default function Post({ ...props }: PostProps) {
   return (
-    <div className="px-4 pt-4 pb-2 flex items-start gap-3 border-b">
+    <div key={props.id} className="flex items-start gap-3 px-5 pb-2 border-b">
       <Image
-        src=""
-        alt=""
-        className="rounded-full w-10 h-10 bg-gray-200"
-        width={10}
-        height={10}
+        src={props.authorImage ? props.authorImage : "/default-profile-pic.png"}
+        alt="avatar"
+        width={40}
+        height={40}
+        className="rounded-full"
       />
       <div className="flex-1">
         <div className="flex items-center gap-2">
-          <span className="font-semibold">Username</span>
-          <span className="text-gray-500">@handle</span>
+          <span className="font-semibold">{props.authorName}</span>
+          <span className="text-gray-500">@{props.authorUsername}</span>
         </div>
-        <p className="mt-2 text-pretty">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
-          nisl id euismod tincidunt, nisl nunc scelerisque nisi, in scelerisque
-          nisl nunc id dui. nunc id dui.nunc id dui.
-        </p>
+        <p className="mt-2 text-pretty">{props.content}</p>
         <div className="mt-2 flex items-center gap-2">
           <Button className="flex gap-1 items-center" variant="ghost">
             <HeartIcon className="h-4 w-4" />
             <span className="sr-only">Like</span>
-            <span className="text-xs"> 400 </span>
+            <span className="text-xs"> 0 </span>
           </Button>
-          
+
           <Button className="flex gap-1 items-center" variant="ghost">
             <ReplyIcon className="h-4 w-4" />
             <span className="sr-only">Comment</span>
-            <span className="text-xs"> 340 </span>
+            <span className="text-xs"> 69 </span>
           </Button>
 
           <Button size="icon" variant="ghost">
