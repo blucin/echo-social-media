972a973c0dfcc81c8de6da590e9c6b34ca857e5d
Akshar Patel <glaivesharp@gmail.com>
2024-02-19 19:47:01 +0530

    Update PostForm styling

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -72,13 +72,13 @@ export default async function PostForm() {
         className={"rounded-full"}
         priority
       />
-      <div className="flex-1 flex flex-col gap-3">
+      <div className="flex-1 flex flex-col gap-1">
         <Textarea
           name="postContent"
           placeholder="What's happening?"
           className="text-xl border-none focus-visible:ring-0"
         />
-        <div className="flex items-center gap-2 text-gray-500 text-sm">
+        <div className="flex items-center gap-2 text-gray-500 text-sm pl-3">
           <Globe2Icon className="h-4 w-4" />
           <span>Everyone can reply</span>
         </div>
