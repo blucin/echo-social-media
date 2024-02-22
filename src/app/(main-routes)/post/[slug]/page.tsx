523f1a0bda6f8b0457a1230e7bf0e412ca1bb44c
Akshar Patel <glaivesharp@gmail.com>
2024-02-22 12:55:59 +0530

    Add PostDetails component

---
+++ src/app/(main-routes)/post/[slug]/page.tsx
@@ -0,0 +1,32 @@
+import Post from "@/components/Post";
+import { getPostById } from "@/db/queries/posts";
+import { XCircleIcon } from "lucide-react";
+
+export default async function PostDetails({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const post = await getPostById(params.slug);
+  if (!post || !post.length || post[0].user.username === null) {
+    return(
+      <main className="flex items-center justify-center h-full">
+        <div className=" text-red-500 flex flex-col items-center gap-4">
+          <XCircleIcon className="h-12 w-12" />
+          <p className="text-pretty">Post not found</p>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Post
+        id={post[0].post.id}
+        authorImage={post[0].user.image}
+        authorName={post[0].user.name}
+        authorUsername={post[0].user.username}
+        content={post[0].post.content}
+      />
+    </main>
+  );
+}
