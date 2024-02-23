eda2be97eb7f754306cc5610b93403689735f8aa
Akshar Patel <glaivesharp@gmail.com>
2024-02-23 22:55:04 +0530

    Refactor user queries and add query by username

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -1,7 +1,31 @@
 import db from "@/lib/db";
-import { user } from "@/drizzle/out/schema";
 import { eq } from "drizzle-orm";
+import { user } from "@/drizzle/out/schema";
 
 export async function getUserById(id: string) {
-  return db.select().from(user).where(eq(user.id, id));
+  return db.query.users.findFirst({
+    columns: {
+      id: true,
+      name: true,
+      username: true,
+      bio: true,
+      image: true,
+      bannerImage: true,
+    },
+    where: eq(user.id, id),
+  });
+}
+
+export async function getUserByUsername(username: string) {
+  return db.query.users.findFirst({
+    columns: {
+      id: true,
+      name: true,
+      username: true,
+      bio: true,
+      image: true,
+      bannerImage: true,
+    },
+    where: eq(user.username, username),
+  });
 }
