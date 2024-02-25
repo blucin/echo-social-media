5ec6ea8a8e533aaea77740a029bb029cbdba372a
Akshar Patel <glaivesharp@gmail.com>
2024-02-25 14:58:34 +0530

    Update RightSideBar component layout

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -5,57 +5,57 @@ import { cn } from "@/lib/utils";
 export default function RightSideBar({ className }: { className?: string }) {
   return (
     <div
-      className={cn(className, "hidden lg:flex flex-col items-end sticky top-0 h-screen ml-10")}
+      className={cn(className, "hidden xl:ml-7 lg:flex flex-col items-center sticky top-0 h-screen")}
     >
       <div className="my-32">
         <p className="font-semibold text-xl mb-5">Who to follow</p>
-        <div className="flex flex-col pr-10 xl:grid grid-cols-2 gap-5">
-          <div className="flex gap-5 items-center">
+        <div className="pr-10 grid grid-cols-2 gap-5">
+          <div className="flex gap-2 items-center">
             <Image
-              src=""
+              src="/default-profile-pic.png"
               alt="profile"
               className="rounded-full w-14 h-14 bg-gray-200"
-              width={14}
-              height={14}
+              width={50}
+              height={50}
             />
             <div>
               <p className="font-semibold">Kanye West</p>
               <p className="text-gray-500">@kanyewest</p>
             </div>
           </div>
-          <div className="flex gap-5 items-center">
+          <div className="flex gap-2 items-center">
             <Image
-              src=""
+              src="/default-profile-pic.png"
               alt="profile"
               className="rounded-full w-14 h-14 bg-gray-200"
-              width={14}
-              height={14}
+              width={50}
+              height={50}
             />
             <div>
               <p className="font-semibold">Kanye West</p>
               <p className="text-gray-500">@kanyewest</p>
             </div>
           </div>
-          <div className="flex gap-5 items-center">
+          <div className="flex gap-2 items-center">
             <Image
-              src=""
+              src="/default-profile-pic.png"
               alt="profile"
               className="rounded-full w-14 h-14 bg-gray-200"
-              width={14}
-              height={14}
+              width={50}
+              height={50}
             />
             <div>
               <p className="font-semibold">Kanye West</p>
               <p className="text-gray-500">@kanyewest</p>
             </div>
           </div>
-          <div className="flex gap-5 items-center">
+          <div className="flex gap-2 items-center">
             <Image
-              src=""
+              src="/default-profile-pic.png"
               alt="profile"
               className="rounded-full w-14 h-14 bg-gray-200"
-              width={14}
-              height={14}
+              width={50}
+              height={50}
             />
             <div>
               <p className="font-semibold">Kanye West</p>
