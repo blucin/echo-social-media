2841599fb81e51408e556b535eef500828836f61
Akshar Patel <glaivesharp@gmail.com>
2024-02-27 17:31:32 +0530

    Add FollowButtonForm component to UserPage

--- src/app/(main-routes)/user/[username]/page.tsx
+++ src/app/(main-routes)/user/[username]/page.tsx
@@ -6,8 +6,8 @@ import { Suspense } from "react";
 import LoadingPosts from "@/components/LoadingPosts";
 import { auth } from "@/auth";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import FollowButtonForm from "@/components/FollowButtonForm";
 
-// TODO: Add followers and following count
 export default async function UserPage({
   params,
   searchParams,
@@ -49,18 +49,29 @@ export default async function UserPage({
             />
           ) : null}
         </AspectRatio>
-        <div className="flex items-center gap-4 relative top-[-20px] left-5 z-20">
-          <Image
-            src={user.image || "/default-profile.png"}
-            alt="profile"
-            width={100}
-            height={100}
-            className="rounded-full"
-          />
-          <div className="flex flex-col pt-3">
-            <h1 className="text-pretty text-2xl font-bold">{user.name}</h1>
-            <p className="text-gray-500">@{user.username}</p>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-4 relative top-[-20px] left-5 z-20">
+            <Image
+              src={user.image || "/default-profile.png"}
+              alt="profile"
+              width={100}
+              height={100}
+              className="rounded-full"
+            />
+            <div>
+              <div className="flex flex-col pt-3">
+                <h1 className="text-pretty text-2xl font-bold">{user.name}</h1>
+                <p className="text-gray-500">@{user.username}</p>
+              </div>
+            </div>
           </div>
+          {session?.user?.id ? (
+            <FollowButtonForm
+              className="mr-5 mb-5 rounded"
+              followeeId={user.id}
+              followerId={session.user.id}
+            />
+          ) : null}
         </div>
         <blockquote className="border-l-4 pl-3 italic text-pretty mx-4">
           {user.bio || "No bio"}
