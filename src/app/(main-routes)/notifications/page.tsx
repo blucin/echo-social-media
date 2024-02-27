36853c9a0be377e0ac17fcc6145b5a28709083b8
Akshar Patel <glaivesharp@gmail.com>
2024-02-27 18:02:34 +0530

    Refactor notification to show no notifications

--- src/app/(main-routes)/notifications/page.tsx
+++ src/app/(main-routes)/notifications/page.tsx
@@ -13,18 +13,28 @@ export default async function NotificationPage() {
 
   return (
     <main>
-      {notifs.map((notif) => {
-        return (
-          <div key={notif.notifId}>
-            <NotificationCard
-              notifId={notif.notifId}
-              notifType={notif.notifType}
-              hasRead={notif.hasRead}
-              fromUser={{ username: notif.fromUser.username ? notif.fromUser.username : "" }}
-            />
-          </div>
-        );
-      })}
+      {notifs.length > 0 ? (
+        notifs.map((notif) => {
+          return (
+            <div key={notif.notifId}>
+              <NotificationCard
+                notifId={notif.notifId}
+                notifType={notif.notifType}
+                hasRead={notif.hasRead}
+                fromUser={{
+                  username: notif.fromUser.username
+                    ? notif.fromUser.username
+                    : "",
+                }}
+              />
+            </div>
+          );
+        })
+      ) : (
+        <div className="flex justify-center items-center h-96">
+          <h1 className="text-xl font-semibold">No notifications</h1>
+        </div>
+      )}
     </main>
   );
 }
