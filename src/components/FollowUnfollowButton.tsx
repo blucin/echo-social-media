7f7e471682a7ad03948a842c35c7ed357d7f7960
Akshar Patel <glaivesharp@gmail.com>
2024-02-27 17:31:06 +0530

    Add FollowUnfollowButton component

---
+++ src/components/FollowUnfollowButton.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Loader2Icon } from "lucide-react";
+import { useTransition } from "react";
+import { handleCreateFollowNotifs } from "@/actions/notifications";
+import { handleDeleteFollower } from "@/actions/followers";
+import { cn } from "@/lib/utils";
+
+type ButtonProps = {
+  followerId: string;
+  followeeId: string;
+  className?: string;
+};
+
+export function UnfollowButton({ ...props }: ButtonProps) {
+  const [isPending, startTransition] = useTransition();
+  const handleSubmit = () => {
+    startTransition(async () => {
+      await handleDeleteFollower({
+        followeeId: props.followeeId,
+      });
+    });
+  };
+  return (
+    <Button
+      onClick={handleSubmit}
+      className={cn("text-base px-5", props.className)}
+      disabled={isPending}
+    >
+      {isPending ? (
+        <>
+          <Loader2Icon className="mr-2 h-4 w-4 animate-spin" /> Please wait{" "}
+        </>
+      ) : (
+        "Unfollow"
+      )}
+    </Button>
+  );
+}
+
+export function FollowButton({ ...props }: ButtonProps) {
+  const [isPending, startTransition] = useTransition();
+  const handleSubmit = () => {
+    startTransition(async () => {
+      await handleCreateFollowNotifs({
+        userId: props.followeeId,
+        fromUserId: props.followerId,
+      });
+    });
+  };
+  return (
+    <Button
+      onClick={handleSubmit}
+      className={cn("text-base px-5", props.className)}
+      disabled={isPending}
+    >
+      {isPending ? (
+        <>
+          <Loader2Icon className="mr-2 h-4 w-4 animate-spin" /> Please wait{" "}
+        </>
+      ) : (
+        "Follow"
+      )}
+    </Button>
+  );
+}
