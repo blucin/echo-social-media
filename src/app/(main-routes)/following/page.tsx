21664e137ed760288074b9ac39bdf54b2f635b27
Akshar Patel <glaivesharp@gmail.com>
2024-02-28 15:54:39 +0530

    Add Following page

---
+++ src/app/(main-routes)/following/page.tsx
@@ -0,0 +1,24 @@
+import Posts from "@/components/Posts";
+import PostForm from "@/components/PostForm";
+import { auth } from "@/auth";
+import { SignInBtn } from "@/components/LoginBtns";
+
+export default async function FollowingPage() {
+  const session = await auth();
+  if (!session) {
+    return (
+      <main className="xl:hidden flex flex-col gap-3 px-5 py-10 border-b">
+        <p className="text-center text-gray-400">Log in to post</p>
+        <div className="flex justify-center">
+          <SignInBtn />
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main className="flex flex-col">
+      <PostForm userImage={session.user.image} />
+      <Posts postsPerLoad={5} followingOnly />
+    </main>
+  );
+}
