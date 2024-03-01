b55f3539d9414c93097a6aab844a6d0def7c7b98
Akshar Patel <glaivesharp@gmail.com>
2024-03-01 17:03:16 +0530

    Refactor Post component UI

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -48,27 +48,20 @@ export default function Post({ ...props }: PostProps) {
           ) : null}
         </Link>
         <p className="mt-2 text-pretty">{props.content}</p>
-        <div className="mt-2 flex items-center gap-2">
-          <Button className="flex gap-1 items-center" variant="ghost">
-            <HeartIcon className="h-4 w-4" />
-            <span className="sr-only">Like</span>
-            <span className="text-xs"> {props.likesCnt ? props.likesCnt : 0} </span>
-          </Button>
-
-          <Button variant="ghost">
+        <div className="flex-1 flex flex-col justify-between">
+          <div>{/* ... placeholder ... */}</div>
+          <Button variant="ghost" className="mt-2 ml-auto">
             <Link
               className="flex gap-1 items-center"
               href={`/post/${props.id}`}
             >
               <ReplyIcon className="h-4 w-4" />
               <span className="sr-only">Comment</span>
-              <span className="text-xs"> {props.commentsCnt ? props.commentsCnt : 0} </span>
+              <span className="text-xs">
+                {" "}
+                {props.commentsCnt ? props.commentsCnt : 0}{" "}
+              </span>
             </Link>
-          </Button>
-
-          <Button size="icon" variant="ghost">
-            <RepeatIcon className="h-4 w-4" />
-            <span className="sr-only">Retweet</span>
           </Button>
         </div>
       </div>
