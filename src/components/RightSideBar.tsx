cd9f0f466b742923a93507e65010c079868a7316
Akshar Patel <glaivesharp@gmail.com>
2024-03-25 19:12:51 +0530

    Refactor RightSideBar component to dynamically render user cards

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -1,67 +1,65 @@
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { getNUsers } from "@/db/queries/users";
+import Link from "next/link";
+
+function UserCard({
+  ...props
+}: {
+  username: string | null;
+  displayName: string | null;
+  userPic: string | null;
+}) {
+  return (
+    <Link
+      className="flex gap-2 items-center hover:bg-gray-100 rounded-lg cursor-pointer p-2"
+      href={`/user/${props.username}`}
+    >
+      <Image
+        src={props.userPic || "/default-profile-pic.png"}
+        alt="profile"
+        className="rounded-full w-14 h-14 bg-gray-200"
+        width={50}
+        height={50}
+      />
+      <div>
+        <p className="font-semibold">{props.displayName}</p>
+        <p className="text-gray-500">@{props.username}</p>
+      </div>
+    </Link>
+  );
+}
 
 // TODO: add right side bar
-export default function RightSideBar({ className }: { className?: string }) {
+export default async function RightSideBar({
+  className,
+}: {
+  className?: string;
+}) {
+  const users = await getNUsers(4);
+
+  if (!users || users.length === 0) {
+    return null;
+  }
+
   return (
     <div
-      className={cn(className, "hidden xl:ml-7 lg:flex flex-col items-center sticky top-0 h-screen")}
+      className={cn(
+        className,
+        "hidden lg:flex flex-col items-center sticky top-0 h-screen"
+      )}
     >
       <div className="my-32">
         <p className="font-semibold text-xl mb-5">Who to follow</p>
-        <div className="pr-10 grid grid-cols-2 gap-5">
-          <div className="flex gap-2 items-center">
-            <Image
-              src="/default-profile-pic.png"
-              alt="profile"
-              className="rounded-full w-14 h-14 bg-gray-200"
-              width={50}
-              height={50}
+        <div className="pr-20 flex flex-col gap-5">
+          {users.map((user) => (
+            <UserCard
+              key={user.username}
+              username={user.username}
+              displayName={user.displayName}
+              userPic={user.profilePic}
             />
-            <div>
-              <p className="font-semibold">Kanye West</p>
-              <p className="text-gray-500">@kanyewest</p>
-            </div>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Image
-              src="/default-profile-pic.png"
-              alt="profile"
-              className="rounded-full w-14 h-14 bg-gray-200"
-              width={50}
-              height={50}
-            />
-            <div>
-              <p className="font-semibold">Kanye West</p>
-              <p className="text-gray-500">@kanyewest</p>
-            </div>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Image
-              src="/default-profile-pic.png"
-              alt="profile"
-              className="rounded-full w-14 h-14 bg-gray-200"
-              width={50}
-              height={50}
-            />
-            <div>
-              <p className="font-semibold">Kanye West</p>
-              <p className="text-gray-500">@kanyewest</p>
-            </div>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Image
-              src="/default-profile-pic.png"
-              alt="profile"
-              className="rounded-full w-14 h-14 bg-gray-200"
-              width={50}
-              height={50}
-            />
-            <div>
-              <p className="font-semibold">Kanye West</p>
-              <p className="text-gray-500">@kanyewest</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
